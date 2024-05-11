01bec93ebbb4a67aa407007e7dfc0ba9587ef891
Emmanuel Ng Cheng Hin <emmanuel_ng@outlook.com>
2024-05-11 16:37:06 -0400

    Simplify base path

--- src/index.php
+++ src/index.php
@@ -2,12 +2,13 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->setBasePath('/memo');
+$app->add(new BasePathMiddleware($app));
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
