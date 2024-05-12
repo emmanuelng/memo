f59f81ac7a406463792ed5f053b280d376d3d7e7
Emmanuel Ng Cheng Hin <emmanuel_ng@outlook.com>
2024-05-12 09:07:37 -0400

    Adds dotenv

--- src/index.php
+++ src/index.php
@@ -1,11 +1,15 @@
 <?php
 
+use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
 
 $app = AppFactory::create();
 $app->add(new BasePathMiddleware($app));
